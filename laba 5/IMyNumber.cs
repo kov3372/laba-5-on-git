de1616980f31ef0f12355595c6b34edcb43fece3
kov3372 <oleg.koval921@gmail.com>
2020-12-17 10:38:50 +0200

    Часть 1

---
+++ laba 5/IMyNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba_5
+{
+    interface IMyNumber <T> where T : IMyNumber<T>
+    {
+        T Add(T b);
+        T Subtract(T b);
+        T Multiply(T b);
+        T Divide(T b);
+    }
+}
---
+++ laba 5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace laba_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testAPlusBSquare(new MyFrac(1, 3), new MyFrac(1, 6));
+            testAPlusBSquare(new MyComplex(1, 3), new MyComplex(1, 6));
+
+           
+                Console.WriteLine(new MyFrac(10,25));
+            
+
+        }
+
+        public static void testAPlusBSquare<T>(T a, T b) where T : IMyNumber<T>
+        {
+            Console.WriteLine("=== Starting testing (a+b)^2=a^2+2ab+b^2 with a = " + a + ", b = " + b + " ===");
+            T aPlusB = a.Add(b);
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("(a + b) = " + aPlusB);
+            Console.WriteLine("(a+b)^2 = " + aPlusB.Multiply(aPlusB));
+            Console.WriteLine(" = = = ");
+            T curr = a.Multiply(a);
+            Console.WriteLine("a^2 = " + curr);
+            T wholeRightPart = curr;
+            curr = a.Multiply(b); // ab
+            curr = curr.Add(curr); // ab + ab = 2ab
+                                   // I’m not sure how to create constant factor "2" in more elegant way,
+                                   // without knowing how IMyNumber is implemented
+            Console.WriteLine("2*a*b = " + curr);
+            wholeRightPart = wholeRightPart.Add(curr);
+            curr = b.Multiply(b);
+            Console.WriteLine("b^2 = " + curr);
+            wholeRightPart = wholeRightPart.Add(curr);
+            Console.WriteLine("a^2+2ab+b^2 = " + wholeRightPart);
+            Console.WriteLine("=== Finishing testing (a+b)^2=a^2+2ab+b^2 with a = " + a + ", b = " + b + " ===");
+        }
+    }
+
+
+}
+    
